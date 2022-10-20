cf7f5ff449e2dcab351b1cf3f1188d2c3134ca51
GMoHughes <georgiamohughes@gmail.com>
2022-10-20 20:20:17 +0100

    adding css styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,35 @@
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://gmohughes.github.io',
     base: '/arcological-website'
 });
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+
+  // all the usual config goes here...
+
+  vite: {
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // path to your scss variables
+          additionalData: `@import "@/assets/scss/global.scss";`
+        }
+      }
+    }
+  }
+};
+@GMoHughes
