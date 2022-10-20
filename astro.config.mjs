e924df32a2e56f1738d6585af2435106e9f7f307
GMoHughes <georgiamohughes@gmail.com>
2022-10-20 21:18:21 +0100

    Updating build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,33 +2,26 @@ import { defineConfig } from 'astro/config';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 
-// https://astro.build/config
-export default defineConfig({
-    site: 'https://gmohughes.github.io',
-    base: '/arcological-website'
-});
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default {
-
-  // all the usual config goes here...
-
-  vite: {
-    resolve: {
-      alias: {
-        '@/': `${path.resolve(__dirname, 'src')}/`
-      }
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          // path to your scss variables
-          additionalData: `@import "@/src/assets/scss/global.scss";`
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://gmohughes.github.io',
+    base: '/arcological-website',
+    vite: {
+      resolve: {
+        alias: {
+          '@/': `${path.resolve(__dirname, 'src')}/`
+        }
+      },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            // path to your scss variables
+            // additionalData: `@import "@/assets/scss/globals.scss";`
+          }
         }
       }
     }
-  }
-};
+});
